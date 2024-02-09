4fe2dda96549f0e49ebbec7f525daa9ea11fa046
Dhyan2404 <159552793+Dhyan2404@users.noreply.github.com>
2024-02-09 20:04:42 +0530

    Add files via upload

---
+++ frist.js
@@ -0,0 +1,6 @@
+let gamenum=39;
+let usernum=prompt("guess the number:");
+while(usernum != gamenum){
+    usernum=prompt("you enterd wrong number.guess right number");
+}
+console.log("congratulations,you guess right number");
